55339b4a4a0042b79fa73aca276218f1daec7f54
jm55815 <malakalapallijitendra@johndeere.com>
2018-11-01 23:05:04 -0500

    feat: finish heap sort

--- sorting/heapsort/modules/heap_sort.py
+++ sorting/heapsort/modules/heap_sort.py
@@ -2,25 +2,42 @@ from basic_operations import left
 from basic_operations import right
 
 
-def max_heapify(A, i):
+def max_heapify(a, i):
     max_index = i
     l = left(i)
     r = right(i)
-    if l < A.heap_size:
-        if A.arr[i] < A.arr[l]:
+    if l < a.heap_size:
+        if a.arr[i] < a.arr[l]:
             max_index = l
 
-    if r < A.heap_size:
-        if A.arr[max_index] < A.arr[r]:
+    if r < a.heap_size:
+        if a.arr[max_index] < a.arr[r]:
             max_index = r
 
     if max_index != i:
-        temp = A.arr[i]
-        A.arr[i] = A.arr[max_index]
-        A.arr[max_index] = temp
+        temp = a.arr[i]
+        a.arr[i] = a.arr[max_index]
+        a.arr[max_index] = temp
         i = max_index
-        max_heapify(A, i)
+        max_heapify(a, i)
     else:
-        print A.arr
         return
 
+
+def build_max_heap(a):
+    for i in reversed(range(0, a.heap_size / 2)):
+        max_heapify(a, i)
+
+
+def sort(a):
+    arr = a.arr
+    build_max_heap(a)
+    for i in range(0, len(arr) - 1):
+        heap_size = a.heap_size
+        temp = arr[heap_size-1]
+        arr[heap_size-1] = arr[0]
+        arr[0] = temp
+        a.heap_size = heap_size - 1
+        max_heapify(a, 0)
+
+    print a.arr
--- sorting/heapsort/test.py
+++ sorting/heapsort/test.py
@@ -1,4 +1,6 @@
 from modules.heap_sort import max_heapify
+from modules.heap_sort import build_max_heap
+from modules.heap_sort import sort
 from classes.a import A
 
 x = A()
@@ -6,4 +8,8 @@ arr = [16, 4, 10, 14, 7, 9, 3, 2, 8, 1]
 x.arr = arr
 x.heap_size = len(arr)
 i = 1
-max_heapify(x, i)
+#max_heapify(x, i)
+
+#build_max_heap(x)
+
+sort(x)
